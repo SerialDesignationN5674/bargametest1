f7e0cd3de07b077043140fea58ad3f754d03cdf0
SerialDesignationN5674 <usupovkostya2013@gmail.com>
2026-01-26 15:47:44 +0500

    cret brek fin
    baaaaaaaaaa

---
+++ break_infinity.js
@@ -0,0 +1,42 @@
+// totally not stealing from ad :)
+class BigNum {
+  constructor(m, e) {
+    this.m = m; // 1 ≤ m < 10
+    this.e = e;
+    this.normalize();
+  }
+
+  normalize() {
+    while (this.m >= 10) {
+      this.m /= 10;
+      this.e += 1;
+    }
+    while (this.m < 1 && this.m !== 0) {
+      this.m *= 10;
+      this.e -= 1;
+    }
+  }
+
+  multiply(other) {
+    return new BigNum(
+      this.m * other.m,
+      this.e + other.e
+    );
+  }
+
+  add(other) {
+    if (this.e !== other.e) {
+      const diff = this.e - other.e;
+      if (diff > 20) return this;       // soft ignore
+      if (diff < -20) return other;
+      return new BigNum(
+        this.m + other.m / Math.pow(10, diff),
+        this.e
+      );
+    }
+    return new BigNum(this.m + other.m, this.e);
+  }
+  toString() {
+    return this.m.toFixed(2) + "e" + this.e;
+  }
+}
